f5dce43b28813fdd9e4da9c278dfa5ae7af540ce
Sam Davis <samdavis@3sidedcube.com>
2025-01-24 09:59:57 +0000

    Convert to Swift Package

--- Baymax/Logger/LogsTool.swift
+++ Baymax/Logger/LogsTool.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2020 3 SIDED CUBE. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 /// A tool for displaying logs collated using `Logger` objects
 public class LogsTool: DiagnosticTool {
